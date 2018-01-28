b87cdbd3c6e99377ee82ed9e1be75c596cf77822
migmartinez <migmart@umich.edu>
2018-01-28 18:24:16 -0500

    part 1 done
    app routes artistinfo, artistlinks, specific/song/<artistname>, and artistform added. part 1 completed

--- SI364W18_HW2.py
+++ SI364W18_HW2.py
@@ -1,7 +1,7 @@
 ## SI 364
 ## Winter 2018
 ## HW 2 - Part 1
-
+## MIGUEL MARTINEZ
 ## This homework has 3 parts, all of which should be completed inside this file (and a little bit inside the /templates directory).
 
 ## Add view functions and any other necessary code to this Flask application code below so that the routes described in the README exist and render the templates they are supposed to (all templates provided are inside the templates/ directory, where they should stay).
@@ -15,6 +15,8 @@ from flask import Flask, request, render_template, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, RadioField, ValidationError
 from wtforms.validators import Required
+import requests
+import json
 
 #####################
 ##### APP SETUP #####
@@ -43,6 +45,35 @@ def hello_world():
 def hello_user(name):
     return '<h1>Hello {0}<h1>'.format(name)
 
+
+	
+@app.route('/artistform')
+def artist_form():	
+	return render_template('artistform.html')
+
+@app.route('/artistinfo', methods = ['GET', 'POST'])
+def a_result():
+	if request.method == 'GET':
+		result = request.args
+		params = {}
+		params['term'] = result.get('artist')
+		params["artistName"] = result.get('artist')
+		resp = requests.get('https://itunes.apple.com/search?', params = params)
+		data = json.loads(resp.text)
+		return render_template('artist_info.html', objects = data['results'])
+
+@app.route('/artistlinks')
+def a_links():
+	return render_template('artist_links.html')
+
+@app.route('/specific/song/<artist_name>')
+def specific_song(artist_name):
+	params = {}
+	params["term"] = artist_name
+	params["limit"] = 3
+	resp = requests.get('https://itunes.apple.com/search?', params = params)
+	data = json.loads(resp.text)
+	return render_template('specific_artist.html', results = data['results'])
 
 if __name__ == '__main__':
     app.run(use_reloader=True,debug=True)
